5b57e0e81d69fd392665e339b4a4d0962da800b5
Manfred2023 <moukatemanfred@gmail.com>
2025-09-25 14:20:36 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,31 @@
+// server.js
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+    cors: {
+        origin: "*",
+    },
+    // IMPORTANT : Forcer uniquement le polling
+    transports: ['polling']
+});
+
+io.on("connection", (socket) => {
+    console.log("Un client connecté:", socket.id);
+
+    socket.on("updatePosition", (data) => {
+        console.log("Nouvelle position:", data);
+        io.emit("positionUpdated", data);
+    });
+
+    socket.on("disconnect", () => {
+        console.log("Client déconnecté:", socket.id);
+    });
+});
+
+server.listen(3000, () => {
+    console.log("🚀 Serveur en écoute sur http://localhost:3000");
+});
